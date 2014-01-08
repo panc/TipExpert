4181343686e05dbc54eef684b1016ddddcee892a
Christoph <Christoph@.(none)>
2014-01-09 00:03:50 +0100

    Added login and user routes.

--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,50 @@
 
-//var async = require('async');
-
 // controlers
-var articles = require('../app/controllers/articles');
+var articles = require('../app/controllers/articles'),
+    users = require('../app/controllers/users');
 
 // Route middlewares
 //var articleAuth = [auth.requiresLogin, auth.article.hasAuthorization]
 
 // expose routes
 module.exports = function(app, passport) {
+
+    // user routes
+    app.get('/login', users.login);
+    //app.get('/signup', users.signup) // do we need signup?
+    app.get('/logout', users.logout);
+    app.post('/users', users.create);
+    app.get('/users/:userId', users.show);
+    
+    app.post('/users/session',
+        passport.authenticate('local', {
+            failureRedirect: '/login',
+            failureFlash: 'Invalid email or password.'
+        }), users.session);
+    app.get('/auth/facebook',
+        passport.authenticate('facebook', {
+            scope: ['email', 'user_about_me'],
+            failureRedirect: '/login'
+        }), users.signin);
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            failureRedirect: '/login'
+        }), users.authCallback);
+
+    app.get('/auth/google',
+        passport.authenticate('google', {
+            failureRedirect: '/login',
+            scope: [
+                'https://www.googleapis.com/auth/userinfo.profile',
+                'https://www.googleapis.com/auth/userinfo.email'
+            ]
+        }), users.signin);
+    app.get('/auth/google/callback',
+        passport.authenticate('google', {
+            failureRedirect: '/login'
+        }), users.authCallback);
+
+    app.param('userId', users.user);
 
     // article routes
     app.get('/articles', articles.index);
