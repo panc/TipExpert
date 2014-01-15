0684cd0aa36b1cca55aa35fcf362b6e4c02ce67f
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-01-15 23:07:06 +0100

    Some beautification.

--- test/test-users.js
+++ test/test-users.js
@@ -1,8 +1,4 @@
 
-/**
- * Module dependencies.
- */
-
 var mongoose = require('mongoose'),
     should = require('should'),
     request = require('supertest'),
@@ -10,21 +6,19 @@ var mongoose = require('mongoose'),
     context = describe,
     User = mongoose.model('User');
 
-var cookies, count;
+var count;
 
-/**
- * Users tests
- */
-
+// users tests
+ 
 describe('Users', function() {
     describe('POST /users', function() {
         describe('Invalid parameters', function() {
             before(function(done) {
                 User.count(function(err, cnt) {
-                    count = cnt
-                    done()
-                })
-            })
+                    count = cnt;
+                    done();
+                });
+            });
 
             it('no email - should respond with errors', function(done) {
                 request(app)
@@ -36,8 +30,8 @@ describe('Users', function() {
                     .expect('Content-Type', /html/)
                     .expect(200)
                     .expect(/Email cannot be blank/)
-                    .end(done)
-            })
+                    .end(done);
+            });
 
             it('no name - should respond with errors', function(done) {
                 request(app)
@@ -49,24 +43,24 @@ describe('Users', function() {
                     .expect('Content-Type', /html/)
                     .expect(200)
                     .expect(/Name cannot be blank/)
-                    .end(done)
-            })
+                    .end(done);
+            });
 
             it('should not save the user to the database', function(done) {
                 User.count(function(err, cnt) {
-                    count.should.equal(cnt)
-                    done()
-                })
-            })
-        })
+                    count.should.equal(cnt);
+                    done();
+                });
+            });
+        });
 
         describe('Valid parameters', function() {
             before(function(done) {
                 User.count(function(err, cnt) {
-                    count = cnt
-                    done()
-                })
-            })
+                    count = cnt;
+                    done();
+                });
+            });
 
             it('should redirect to /articles', function(done) {
                 request(app)
@@ -79,28 +73,28 @@ describe('Users', function() {
                     .expect('Location', /\//)
                     .expect(302)
                     .expect(/Moved Temporarily/)
-                    .end(done)
-            })
+                    .end(done);
+            });
 
             it('should insert a record to the database', function(done) {
                 User.count(function(err, cnt) {
-                    cnt.should.equal(count + 1)
-                    done()
-                })
-            })
+                    cnt.should.equal(count + 1);
+                    done();
+                });
+            });
 
             it('should save the user to the database', function(done) {
                 User.findOne({ username: 'foobar' }).exec(function(err, user) {
-                    should.not.exist(err)
-                    user.should.be.an.instanceOf(User)
-                    user.email.should.equal('foobar@example.com')
-                    done()
-                })
-            })
-        })
-    })
+                    should.not.exist(err);
+                    user.should.be.an.instanceOf(User);
+                    user.email.should.equal('foobar@example.com');
+                    done();
+                });
+            });
+        });
+    });
 
     after(function(done) {
-        require('./helper').clearDb(done)
-    })
+        require('./helper').clearDb(done);
+    });
 });
