5614c94993bde613c7b8504d6f4bcbd2f4048e37
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-01-22 15:39:05 +0100

    Added league test.

---
+++ test/test-leagues.js
@@ -0,0 +1,81 @@
+
+var mongoose = require('mongoose'),
+    should = require('should'),
+    request = require('supertest'),
+    app = require('../app'),
+    context = describe,
+    League = mongoose.model('League');
+
+var count;
+
+// league tests
+ 
+describe('Leagues', function() {
+    describe('POST /leagues', function() {
+        describe('Invalid parameters', function() {
+            before(function(done) {
+                League.count(function(err, cnt) {
+                    count = cnt;
+                    done();
+                });
+            });
+
+            it('no name - should respond with errors', function(done) {
+                request(app)
+                    .post('/leagues')
+                    .field('name', '')
+                    .expect('Content-Type', /html/)
+                    .expect(200)
+                    .expect(/Email cannot be blank/)
+                    .end(done);
+            });
+
+            it('should not save the user to the database', function(done) {
+                League.count(function(err, cnt) {
+                    count.should.equal(cnt);
+                    done();
+                });
+            });
+        });
+
+        describe('Valid parameters', function() {
+            before(function(done) {
+                League.count(function(err, cnt) {
+                    count = cnt;
+                    done();
+                });
+            });
+
+            it('should redirect to /articles', function(done) {
+                request(app)
+                    .post('/leagues')
+                    .field('name', 'Bundesliga')
+                    .expect('Content-Type', /json/)
+                    .expect('Location', /\//)
+                    .expect(302)
+                    .expect(/Moved Temporarily/)
+                    .end(done);
+            });
+
+            it('should insert a record to the database', function(done) {
+                League.count(function(err, cnt) {
+                    cnt.should.equal(count + 1);
+                    done();
+                });
+            });
+
+            it('should save the league to the database', function(done) {
+                League.findOne({ username: 'Bundesliga' }).exec(function(err, league) {
+                    should.not.exist(err);
+                    league.should.be.an.instanceOf(League);
+                    league.name.should.equal('Bundesliga');
+                    done();
+                });
+            });
+        });
+    });
+
+    after(function(done) {
+        require('./helper').clearDb(done);
+    });
+});
