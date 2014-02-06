a2b5b21d40f659ebc73970abfd20404d81772b7c
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-02-06 21:20:29 +0100

    Further work.

--- public/js/app.js
+++ public/js/app.js
@@ -5,33 +5,43 @@
 var tipExpert = angular.module('tipExpert', ['ngRoute', 'home', 'user']);
 
 tipExpert.config(['$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
+
+    var accessLevels = {
+        admin: 1,
+        user: 2,
+        public: 3
+    };
+
     $routeProvider
     
-    // user routes
-    .when('/user', {
-        templateUrl: 'views/user/user.html',
-        controller: 'userController'
-    })
-    .when('/user/:userId', {
-        templateUrl: '/views/user/profile.html',
-        controller: 'userController'
-    })
-    .when('/login', {
-        templateUrl: '/views/user/login.html',
-        controller: 'loginController'
-    })
+        // user routes
+        .when('/user', {
+            templateUrl: 'views/user/user.html',
+            controller: 'userController',
+            access: accessLevels.user
+        })
+        .when('/user/:userId', {
+            templateUrl: '/views/user/profile.html',
+            controller: 'userController',
+            access: accessLevels.user
+        })
+        .when('/login', {
+            templateUrl: '/views/user/login.html',
+            controller: 'loginController',
+            access: accessLevels.public
+        })
     
-    // home route
-    .when('/', {
-        templateUrl: '/views/index.html',
-        controller: 'homeController'
-    })
-    .otherwise({
-        redirectTo: '/'
-    });
+        // home route
+        .when('/', {
+            templateUrl: '/views/index.html',
+            controller: 'homeController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 
     $locationProvider.html5Mode(true);
-    
+
     $httpProvider.interceptors.push(function($q, $location) {
         return {
             'responseError': function(response) {
@@ -45,3 +55,15 @@ tipExpert.config(['$routeProvider', '$locationProvider', '$httpProvider', functi
         };
     });
 }]);
+
+//.run(['$rootScope', '$location', '$http', 'Auth', function($rootScope, $location, $http, Auth) {
+//
+//    $rootScope.$on("$routeChangeStart", function(event, next, current) {
+//        $rootScope.error = null;
+//        if (!Auth.authorize(next.access)) {
+//            if (Auth.isLoggedIn()) $location.path('/');
+//            else $location.path('/login');
+//        }
+//    });
+//
+//}]);
---
+++ public/js/services/authService.js
@@ -0,0 +1,72 @@
+'use strict';
+
+angular.module('tipExpert', [])
+    .factory('Auth', function($http, $cookieStore) {
+
+        var accessLevels = {
+            admin: 1,
+            user: 2,
+            public: 3
+        };
+        
+        var userRoles = {
+            admin: 1,
+            user: 2,
+            public: 3
+        };
+        
+        var currentUser = $cookieStore.get('user') || { username: '', role: userRoles.public };
+
+        $cookieStore.remove('user');
+
+        function changeUser(user) {
+            _.extend(currentUser, user);
+        }
+
+        ;
+
+        return {
+            authorize: function(accessLevel, role) {
+                if (role === undefined)
+                    role = currentUser.role;
+
+                if (userRoles.admin)
+                    return true;
+                
+                if (accessLevel == accessLevels.user && (role == userRoles.admin || role == userRoles.user))
+                    return true;
+
+                return accessLevel == accessLevels.public;
+            },
+            isLoggedIn: function(user) {
+                if (user === undefined)
+                    user = currentUser;
+                
+                return user.role == userRoles.user || user.role == userRoles.admin;
+            },
+//            register: function(user, success, error) {
+//                $http.post('/register', user).success(function(res) {
+//                    changeUser(res);
+//                    success();
+//                }).error(error);
+//            },
+            login: function(user, success, error) {
+                $http.post('/login', user).success(function(user) {
+                    changeUser(user);
+                    success(user);
+                }).error(error);
+            },
+            logout: function(success, error) {
+                $http.post('/logout').success(function() {
+                    changeUser({
+                        username: '',
+                        role: userRoles.public
+                    });
+                    success();
+                }).error(error);
+            },
+            accessLevels: accessLevels,
+            userRoles: userRoles,
+            user: currentUser
+        };
+    });
