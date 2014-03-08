9ec9e5a023e5aacbc8b6c60faeac11c4e139ec0a
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-03-08 07:59:12 +0100

    Added support for showing and adding new leagues.

--- public/modules/match/matchPresenter.js
+++ public/modules/match/matchPresenter.js
@@ -6,23 +6,20 @@ var match = angular.module('tipExpert.match', []);
 
 match.controller('matchController', ['$http', '$scope', function($http, $scope)  {
 
-    $scope.roles = [
-        { name: 'Admin', index: 1 },
-        { name: 'User', index: 2 }
-    ];
+    $scope.leagues = [];
+    $scope.newLeague = { name: ''};
 
-//    $scope.save = function() {
-//        angular.forEach($scope.users, function(user) {
-//            $http.put('api/user/' + user._id, user)
-//                .success(function(data, status, headers, config) {
-//                    alert('success');
-//                })
-//                .error(function(data, status, headers, config) {
-//                    // todo   
-//                });
-//        });
-//    };
-//    
+    $scope.addLeague = function() {
+        
+        $http.post('api/leagues/', $scope.newLeague)
+            .success(function(data, status, headers, config) {
+                $scope.leagues.push(data);
+            })
+            .error(function(data, status, headers, config) {
+                // todo   
+            });
+    };
+    
     $http.get('/api/leagues').
         success(function(data, status, headers, config) {
             $scope.leagues = data;
