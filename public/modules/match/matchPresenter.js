1f6fc6f6cc71740d673ff64a4d2ba5fbfa730ad6
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-03-09 11:41:02 +0100

    Edit support for leagues.

--- public/modules/match/matchPresenter.js
+++ public/modules/match/matchPresenter.js
@@ -13,7 +13,7 @@ match.controller('matchController', ['$http', '$scope', function($http, $scope) 
         
         $http.post('api/leagues/', $scope.newLeague)
             .success(function(data, status, headers, config) {
-                $scope.leagues.push(data);
+                $scope.leagues.push({ item: data, editorEnabled: false });
             })
             .error(function(data, status, headers, config) {
                 // todo   
@@ -23,7 +23,7 @@ match.controller('matchController', ['$http', '$scope', function($http, $scope) 
 
     $scope.removeLeague = function(league) {
 
-        $http.delete('api/leagues/' + league._id)
+        $http.delete('api/leagues/' + league.item._id)
             .success(function(data, status, headers, config) {
 
                 var index = $scope.leagues.indexOf(league);
@@ -34,10 +34,43 @@ match.controller('matchController', ['$http', '$scope', function($http, $scope) 
                 alert(data);
             });
     };
+
+    $scope.editLeague = function(league) {
+
+        league.editorEnabled = true;
+        league.editableName = league.item.name;
+    };
     
-    $http.get('/api/leagues').
-        success(function(data, status, headers, config) {
-            $scope.leagues = data;
+    $scope.cancelEditLeague = function(league) {
+
+        league.editorEnabled = false;
+    };
+    
+    $scope.saveLeague = function(league) {
+
+        league.item.name = league.editableName;
+
+        $http.put('/api/leagues/' + league.item._id, league.item)
+            .success(function(data, status, headers, config) {
+            
+                league.editorEnabled = false;
+            })
+            .error(function(data, status, headers, config) {
+                // todo
+                alert(data);
+            });
+    };
+    
+    $http.get('/api/leagues')
+        .success(function(data, status, headers, config) {
+
+            var leagues = [];
+
+            angular.forEach(data, function(league) {
+                leagues.push({ item: league, editorEnabled: false });
+            });
+            
+            $scope.leagues = leagues;
         })
         .error(function(data, status, headers, config) {
             // todo
