3bdabe863d1e60bd203337ae3297e49e3f61020b
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-03-12 23:14:50 +0100

    Fixed tests.

--- test/helper.js
+++ test/helper.js
@@ -1,7 +1,7 @@
 
 var mongoose = require('mongoose'),
     async = require('async'),
-    Article = mongoose.model('Article'),
+    League = mongoose.model('League'),
     User = mongoose.model('User');
 
 exports.clearDb = function(done) {
@@ -10,7 +10,7 @@ exports.clearDb = function(done) {
             User.collection.remove(cb);
         },
         function(cb) {
-            Article.collection.remove(cb);
+            League.collection.remove(cb);
         }
     ], done);
 };
