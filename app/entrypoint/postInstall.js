84022080f28941d782994062bca999fba4e4cf05
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-03-12 20:35:23 +0100

    Fixed postInstall.js

--- app/entrypoint/postInstall.js
+++ app/entrypoint/postInstall.js
@@ -8,7 +8,7 @@ if (env === 'heroku' || env === 'production') {
 
     var exec = require('child_process').exec;
     
-    child = exec('../../node_modules/.bin/bower install', function(error, stdout, stderr) {
+    child = exec('./node_modules/.bin/bower install', function(error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
         if (error !== null) {
