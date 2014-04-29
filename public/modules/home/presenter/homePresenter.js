356a48edd26dd9f88155c21efc122b375e887aa1
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-04-30 00:02:19 +0200

    Further work on design for the landing page.

--- public/modules/home/presenter/homePresenter.js
+++ public/modules/home/presenter/homePresenter.js
@@ -2,7 +2,25 @@
 
 var homeModule = angular.module('tipExpert.home', []);
 
-homeModule.controller('homeController', ['$scope', function($scope) {
+homeModule.controller('homeController', [
+    '$scope', 'Auth', function($scope, Auth) {
 
-   
-}]);
+        $scope.user = {
+            name: '',
+            email: '',
+            password: ''
+        };
+
+        $scope.signup = function() {
+            $scope.submitted = true;
+
+            if ($scope.submitForm.$invalid)
+                return;
+
+            Auth.signup($scope.user, function() {
+                    $state.go('home');
+                },
+                toast.error);
+        };
+    }
+]);
